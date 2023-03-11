2b0d51ff74a6c156abb69d1475598ec254b9fb92
EduChappie <chappiefevplus@gmail.com>
2023-03-11 14:58:14 -0300

    pequenas adições

--- page_1/script.js
+++ page_1/script.js
@@ -22,3 +22,17 @@ gallery.addEventListener('mousemove', (key) => {
         easing: "ease"
     })
 });
+
+const main = document.querySelector("main");
+main.addEventListener("mousemove", (key) => {
+    const text = document.querySelector("div.start");
+
+    text.style.top = "45%";
+    text.style.left = "45%";
+    text.style.zIndex = "0"
+})
+
+
+document.getElementsByClassName("box")[0].addEventListener('click', (ck) => {
+    window.alert("funcionou!");
+})
